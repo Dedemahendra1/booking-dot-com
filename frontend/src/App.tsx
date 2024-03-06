42a8c889ae72d5d29379f4acc2462682d1e47234
Dede <dedemahendra201@gmail.com>
2024-03-06 21:25:53 +0700

    add hotel feature

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,18 @@
 import {
   BrowserRouter as Router,
   Route,
-  Routes
+  Routes,
+  Navigate
 } from "react-router-dom";
 import Layout from "./layouts/Layout";
 import Register from "./pages/Register";
 import SignIn from "./pages/SignIn";
+import AddHotel from "./pages/AddHotel";
+import { useAppContext } from "./contexts/AppContext";
 
 
 const App = () => {
+  const {isLoggedIn} = useAppContext();
   return (
     <Router>
       <Routes>
@@ -16,6 +20,12 @@ const App = () => {
         <Route path="/search" element={<Layout><p>SearchPage</p></Layout>} />
         <Route path="/register" element={<Layout><Register /></Layout>} />
         <Route path="/sign-in" element={<Layout><SignIn /></Layout>} />
+        {isLoggedIn && (
+          <>
+              <Route path="/add-hotel" element={<Layout><AddHotel/></Layout>} />
+          </>
+        )}
+        <Route path="*" element={<Navigate to="/" />} />
       </Routes>
     </Router>
   )
--- frontend/src/api-client.ts
+++ frontend/src/api-client.ts
@@ -60,3 +60,18 @@ const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || "";
       throw new Error("Error during sign out");
     }
   };
+
+  export const addMyHotel = async (hotelFormData: FormData) => {
+    const response = await fetch(`${API_BASE_URL}/api/my-hotels`, {
+      method: "POST",
+      credentials: "include",
+      body: hotelFormData,
+    });
+  
+    if (!response.ok) {
+      throw new Error("Failed to add hotel");
+    }
+  
+    return response.json();
+  };
+  
